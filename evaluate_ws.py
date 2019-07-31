e10e747506998d4ed5bece097b76fef7aad8330e
cfhaiteeh <1399162309@qq.com>
2019-07-31 19:25:59 +0800

    xx commit

--- evaluate_ws.py
+++ evaluate_ws.py
@@ -11,7 +11,7 @@ import os
 # Jan1 2019. Wonseok. Path info has added to original wikisql/evaluation.py
 # Only need to add "query" (essentially "sql" in original data) and "table_id" while constructing file.
 #wanghuajiess
-#sss
+#sssxxx
 if __name__ == '__main__':
 
     # Hyper parameters
